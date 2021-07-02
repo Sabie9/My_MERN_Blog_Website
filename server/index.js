29b0d8db4007cbb4240649c622b527d538f905db
Saayan Biswas <42497416+Sabie9@users.noreply.github.com>
2021-07-03 00:17:45 +0530

    final tweaking

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://saayan99:CEc1eHhGwK07DDZb@cluster0.uvutu.mongodb.net/designlab?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://saayan99:123123@cluster0.uvutu.mongodb.net/designlab?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
